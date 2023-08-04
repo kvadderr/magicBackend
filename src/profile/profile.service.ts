e83062114a94638c6fe2c695ac351972b2890efe
Dartarus61 <07kirka2002@gmail.com>
2023-08-04 12:24:40 +0300

    add: sort by time for inventory

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -35,6 +35,7 @@ export class ProfileService {
           serverType: true,
         },
       });
+      result.sort((a, b) => a.createdAt.valueOf() - b.createdAt.valueOf());
       return {
         result: result.slice((page - 1) * count, page * count),
         pages: result.length > count ? Math.ceil(result.length / count) : 1,
