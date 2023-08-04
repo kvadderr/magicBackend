3c91f7fcedda64e108ca9007c05ff32e129224c8
Dartarus61 <07kirka2002@gmail.com>
2023-08-04 15:34:02 +0300

    fix: DI module in notification

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -5,10 +5,11 @@ import { HttpModule } from '@nestjs/axios';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UsersModule } from 'src/users/users.module';
 import { TokenModule } from 'src/token/token.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [NotificationController],
   providers: [NotificationService, PrismaService],
-  imports: [HttpModule, UsersModule, TokenModule],
+  imports: [HttpModule, UsersModule, TokenModule, JwtModule],
 })
 export class NotificationModule {}
